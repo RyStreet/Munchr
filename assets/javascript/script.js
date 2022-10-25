f09e08032797c8bac531482753c7e495f6ba84c9
John W <john.weaver94@utexas.edu>
2022-10-24 19:41:51 -0500

    api basic info!

---
+++ assets/javascript/script.js
@@ -0,0 +1,27 @@
+//getApi function is called when the fetchButton is clicked
+searchTerm="broccoli"
+beepo="Kahlua"
+function getApi() {
+  // Insert the API url to get a list of your repos
+  var requestUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?i='+searchTerm;
+
+  fetch(requestUrl)
+    .then(function (response) {
+      console.log(response.json());
+      // return response.json();
+    })
+}
+function getApi2() {
+  // Insert the API url to get a list of your repos
+  var requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i='+beepo;
+
+  fetch(requestUrl)
+    .then(function (response) {
+      console.log(response.json());
+      // return response.json();
+    })
+}
+
+
+getApi();
+getApi2();
